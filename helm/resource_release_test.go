c9ff25ea7ab616f257fe5473d4b7650e7fe23313
Maartje Eyskens <maartje@eyskens.me>
2019-03-07 19:39:00 +0100

    Try to valid escape
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- helm/resource_release_test.go
+++ helm/resource_release_test.go
@@ -122,7 +122,7 @@ func TestAccResourceRelease_setStringValues(t *testing.T) {
 				resource.TestCheckResourceAttr("helm_release.test", "metadata.0.values", "test: 10.0.0.0/32,10.0.0.1/32,10.0.0.2/32\n"),
 			),
 		}, {
-			Config: testAccHelmReleaseConfigSetString(testResourceName, testNamespace, testResourceName, "0.6.3", "10.0.0.0\\/32,10.0.0.1\\/32,10.0.0.2\\/32"),
+			Config: testAccHelmReleaseConfigSetString(testResourceName, testNamespace, testResourceName, "0.6.3", "10.0.0.0\\\/32,10.0.0.1\\\/32,10.0.0.2\\\/32"),
 			Check: resource.ComposeAggregateTestCheckFunc(
 				resource.TestCheckResourceAttr("helm_release.test", "status", "DEPLOYED"),
 				resource.TestCheckResourceAttr("helm_release.test", "metadata.0.values", "test: 10.0.0.0/32,10.0.0.1/32,10.0.0.2/32\n"),
