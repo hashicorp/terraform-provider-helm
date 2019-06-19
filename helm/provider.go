4cdce557ef515783c1683fb14b37969d089ca6fa
Rafael Porres Molina <rafa@sourced.tech>
2019-06-19 14:42:54 +0200

    Update to Helm 2.14.1 (#294)
    * go get k8s.io/helm...@v2.14.1
    
    Signed-off-by: Rafael Porres Molina <rafa@sourced.tech>
    
    * go mod tidy
    
    Signed-off-by: Rafael Porres Molina <rafa@sourced.tech>
    
    * git rm -f -r -q vendor && go mod vendor
    
    Signed-off-by: Rafael Porres Molina <rafa@sourced.tech>
    
    * Tiller is gcr.io/kubernetes-helm/tiller:v2.14.1
    
    Signed-off-by: Rafael Porres Molina <rafa@sourced.tech>

--- helm/provider.go
+++ helm/provider.go
@@ -70,7 +70,7 @@ func Provider() terraform.ResourceProvider {
 			"tiller_image": {
 				Type:        schema.TypeString,
 				Optional:    true,
-				Default:     "gcr.io/kubernetes-helm/tiller:v2.14.0",
+				Default:     "gcr.io/kubernetes-helm/tiller:v2.14.1",
 				Description: "Tiller image to install.",
 			},
 			"service_account": {
