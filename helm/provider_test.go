c5599dda5664561b10b3d41ee4748366633f5f89
Alex Pilon <apilon@hashicorp.com>
2019-09-10 22:34:01 -0400

    remove usage of config pkg update terraform sdk to v0.12.8

--- helm/provider_test.go
+++ helm/provider_test.go
@@ -8,7 +8,6 @@ import (
 	"os"
 	"testing"
 
-	"github.com/hashicorp/terraform/config"
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/terraform"
@@ -76,16 +75,11 @@ func testAccProviderHelmInit(t *testing.T, enabled bool) (err error) {
 
 	log.Printf("[INFO] Test: Using %s as helm home", helmHome)
 
-	c, err := config.NewRawConfig(map[string]interface{}{
+	testProvider := Provider().(*schema.Provider)
+	err = testProvider.Configure(terraform.NewResourceConfigRaw(map[string]interface{}{
 		"home":           helmHome,
 		"init_helm_home": enabled,
-	})
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	testProvider := Provider().(*schema.Provider)
-	err = testProvider.Configure(terraform.NewResourceConfig(c))
+	}))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -104,7 +98,7 @@ func testAccPreCheck(t *testing.T) {
 	log.Printf("[INFO] Test: Using %s as helm home", testAccHelmHome)
 	os.Setenv("HELM_HOME", testAccHelmHome)
 
-	err := testAccProvider.Configure(terraform.NewResourceConfig(nil))
+	err := testAccProvider.Configure(terraform.NewResourceConfigRaw(nil))
 	if err != nil {
 		t.Fatal(err)
 	}
