dcebd027dca288872dc23a5d20323c6b4f3f33b6
Aaron Mell <amell@lumindigital.com>
2019-11-22 20:22:39 -0600

    Removed tests from provider that were no longer needed.

--- helm/provider_test.go
+++ helm/provider_test.go
@@ -36,66 +36,11 @@ func init() {
 	}
 }
 
-// 	var err error
-// 	testAccHelmHome, err = ioutil.TempDir("", "terraform-acc-test-helm-")
-// 	if err != nil {
-// 		log.Printf("[ERROR] Failed to create new temporary directory for use as helm home: %s", err)
-// 	}
-// }
-
 func TestProvider(t *testing.T) {
 	if err := Provider().(*schema.Provider).InternalValidate(); err != nil {
 		t.Fatalf("err: %s", err)
 	}
 }
-
-// func TestAccProviderHelmInitEnabled(t *testing.T) {
-// 	err := testAccProviderHelmInit(t, true)
-// 	if err != nil {
-// 		t.Fatal("helm home should have been initialized", err)
-// 	}
-// }
-
-// func TestAccProviderHelmInitDisabled(t *testing.T) {
-// 	err := testAccProviderHelmInit(t, false)
-// 	if err != io.EOF {
-// 		t.Fatal("helm home should not have been initialized", err)
-// 	}
-// }
-
-// func testAccProviderHelmInit(t *testing.T, enabled bool) (err error) {
-// 	if os.Getenv(resource.TestEnvVar) == "" {
-// 		t.Skip(fmt.Sprintf(
-// 			"Acceptance tests skipped unless env '%s' set", resource.TestEnvVar))
-// 		return
-// 	}
-
-// 	helmHome, err := ioutil.TempDir("", "terraform-acc-test-helm-")
-// 	if err != nil {
-// 		t.Fatalf("Failed to create new temporary directory for use as helm home: %s", err)
-// 	}
-// 	defer os.RemoveAll(helmHome)
-
-// 	log.Printf("[INFO] Test: Using %s as helm home", helmHome)
-
-// 	testProvider := Provider().(*schema.Provider)
-// 	err = testProvider.Configure(terraform.NewResourceConfigRaw(map[string]interface{}{
-// 		"home":           helmHome,
-// 		"init_helm_home": enabled,
-// 	}))
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-
-// 	f, err := os.Open(helmHome)
-// 	defer f.Close()
-// 	if err != nil {
-// 		t.Fatal("Failed to access helm home directory")
-// 	}
-
-// 	_, err = f.Readdirnames(1)
-// 	return
-// }
 
 func testAccPreCheck(t *testing.T, namespace string) {
 	err := setK8Client()
