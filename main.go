ce0fd07ee8584befa34a7d8344e5f3aff237dca9
Peter Wilcsinszky <peterwilcsinszky@gmail.com>
2020-06-15 21:46:45 +0200

    disable writing to stderr with klog (#505)

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 	"github.com/hashicorp/terraform-provider-helm/helm"
+	"k8s.io/klog"
 )
 
 func main() {
+	flag.Parse()
+	klogFlags := flag.NewFlagSet("klog", flag.ExitOnError)
+	klog.InitFlags(klogFlags)
+	err := klogFlags.Set("logtostderr", "false")
+	if err != nil {
+		panic(err)
+	}
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: helm.Provider,
 	})
