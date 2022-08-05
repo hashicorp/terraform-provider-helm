8625d22e93990bcea85c0bfe785bd1ce752eec50
Volodymyr Kuznetsov <vova@cyberhaven.com>
2022-08-05 11:45:39 -0700

    added test for the repository_password_env option

--- helm/resource_release_test.go
+++ helm/resource_release_test.go
@@ -1404,6 +1404,41 @@ func TestAccResourceRelease_OCI_login(t *testing.T) {
 	})
 }
 
+func TestAccResourceRelease_OCI_login_env(t *testing.T) {
+	name := randName("oci")
+	namespace := createRandomNamespace(t)
+	defer deleteNamespace(t, namespace)
+
+	ociRegistryURL, shutdown := setupOCIRegistry(t, true)
+	defer shutdown()
+
+	envVar := "TestAccResourceRelease_OCI_login_env_password"
+	// os.Setenv(envVar, "terraform")
+
+	resource.Test(t, resource.TestCase{
+		PreCheck: func() {
+			testAccPreCheck(t)
+		},
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckHelmReleaseDestroy(namespace),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccHelmReleaseConfig_OCI_login_env_multiple(testResourceName, namespace, name, ociRegistryURL, "1.2.3", "hashicorp", envVar),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("helm_release.test1", "metadata.0.name", name+"1"),
+					resource.TestCheckResourceAttr("helm_release.test1", "metadata.0.namespace", namespace),
+					resource.TestCheckResourceAttr("helm_release.test1", "metadata.0.version", "1.2.3"),
+					resource.TestCheckResourceAttr("helm_release.test1", "status", release.StatusDeployed.String()),
+					resource.TestCheckResourceAttr("helm_release.test2", "metadata.0.name", name+"2"),
+					resource.TestCheckResourceAttr("helm_release.test2", "metadata.0.namespace", namespace),
+					resource.TestCheckResourceAttr("helm_release.test2", "metadata.0.version", "1.2.3"),
+					resource.TestCheckResourceAttr("helm_release.test2", "status", release.StatusDeployed.String()),
+				),
+			},
+		},
+	})
+}
+
 func testAccHelmReleaseConfig_OCI(resource, ns, name, repo, version string) string {
 	return fmt.Sprintf(`
 		resource "helm_release" "%s" {
@@ -1439,6 +1474,31 @@ func testAccHelmReleaseConfig_OCI_login_multiple(resource, ns, name, repo, versi
 		   repository_password = %[7]q
 	   }
 	`, resource, name, ns, repo, version, username, password)
+}
+
+func testAccHelmReleaseConfig_OCI_login_env_multiple(resource, ns, name, repo, version, username, password_env string) string {
+	return fmt.Sprintf(`
+		resource "helm_release" "%s1" {
+ 			name        = "%s1"
+			namespace   = %q
+			repository  = %q
+			version     = %q
+			chart       = "test-chart"
+
+			repository_username = %q
+			repository_password_env = %q
+		}
+		resource "helm_release" "%[1]s2" {
+			name       = "%[2]s2"
+		   namespace   = %[3]q
+		   repository  = %[4]q
+		   version     = %[5]q
+		   chart       = "test-chart"
+
+		   repository_username = %[6]q
+		   repository_password_env = %[7]q
+	   }
+	`, resource, name, ns, repo, version, username, password_env)
 }
 
 func testAccHelmReleaseConfig_OCI_chartName(resource, ns, name, chartName, version string) string {
