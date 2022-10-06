cf385d2b74533654a318f1913dbf4cb18eb49603
John Houston <jhouston@hashicorp.com>
2022-10-06 00:17:29 -0400

    Add regression test for unknown values bug

--- helm/resource_release.go
+++ helm/resource_release.go
@@ -807,6 +807,17 @@ func resourceDiff(ctx context.Context, d *schema.ResourceDiff, meta interface{})
 	debug("%s Release validated", logID)
 
 	if m.ExperimentEnabled("manifest") {
+		// NOTE we need to check that the values supplied to the release are
+		// fully known at plan time otherwise we can't supply them to the
+		// action to perform a dry run
+		if !valuesKnown(d) {
+			// NOTE it would be nice to surface a warning diagnostic here
+			// but this is not possible with the SDK
+			debug("not all values are known, skipping dry run to render manifest")
+			d.SetNewComputed("manifest")
+			return d.SetNewComputed("version")
+		}
+
 		var postRenderer postrender.PostRenderer
 		if cmd := d.Get("postrender.0.binary_path").(string); cmd != "" {
 			av := d.Get("postrender.0.args")
@@ -846,10 +857,6 @@ func resourceDiff(ctx context.Context, d *schema.ResourceDiff, meta interface{})
 			install.CreateNamespace = d.Get("create_namespace").(bool)
 			install.PostRenderer = postRenderer
 
-			// if !valuesWhollyKnown(d) {
-			// 	d.SetNewComputed("manifest")
-			// 	return d.SetNewComputed("version")
-			// }
 			values, err := getValues(d)
 			if err != nil {
 				return fmt.Errorf("error getting values: %v", err)
@@ -1374,18 +1381,18 @@ func resultToError(r *action.LintResult) error {
 	return fmt.Errorf("malformed chart or values: \n\t%s", strings.Join(messages, "\n\t"))
 }
 
-func valuesWhollyKnown(d *schema.ResourceDiff) bool {
+// valuesKnown returns true if all of the values supplied to the release are known at plan time
+func valuesKnown(d *schema.ResourceDiff) bool {
 	rawPlan := d.GetRawPlan()
-
-	if !rawPlan.GetAttr("values").IsWhollyKnown() {
-		return false
+	checkAttributes := []string{
+		"values",
+		"set",
+		"set_sensitive",
 	}
-	if !rawPlan.GetAttr("set").IsWhollyKnown() {
-		return false
+	for _, attr := range checkAttributes {
+		if !rawPlan.GetAttr(attr).IsWhollyKnown() {
+			return false
+		}
 	}
-	if !rawPlan.GetAttr("set_sensitive").IsWhollyKnown() {
-		return false
-	}
-
 	return true
 }
