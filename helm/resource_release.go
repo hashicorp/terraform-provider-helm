98d563da5de549d14bc8e9ac59be2b4ad5a3a836
Aleksandr Rybolovlev <sacha.rybolovlev@hashicorp.com>
2023-08-17 09:32:26 +0200

    Add name field validation of the helm_release resource

--- helm/resource_release.go
+++ helm/resource_release.go
@@ -70,10 +70,11 @@ func resourceRelease() *schema.Resource {
 		CustomizeDiff: resourceDiff,
 		Schema: map[string]*schema.Schema{
 			"name": {
-				Type:        schema.TypeString,
-				Required:    true,
-				ForceNew:    true,
-				Description: "Release name.",
+				Type:         schema.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.StringLenBetween(1, 53),
+				Description:  "Release name.",
 			},
 			"repository": {
 				Type:        schema.TypeString,
