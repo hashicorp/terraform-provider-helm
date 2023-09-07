633db223ca94b7ba7c9730b9f133ae2fcfb02726
BBBmau <alvarez.mauriciotm@gmail.com>
2023-09-07 14:36:37 -0700

    add apiversion validator for provider

--- helm/provider.go
+++ helm/provider.go
@@ -2,12 +2,13 @@ package provider
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
-	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
 	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
@@ -106,6 +107,28 @@ func (p *helmProvider) Schema(ctx context.Context, _ provider.SchemaRequest, res
 							Optional:    true,
 							Description: "URL to the proxy to be used for all API requests",
 						},
+						"exec": schema.ListNestedAttribute{
+							NestedObject: schema.NestedAttributeObject{
+								Attributes: map[string]schema.Attribute{
+									"api_version": schema.StringAttribute{
+										Required:   true,
+										Validators: []validator.String{versionValidator{}},
+									},
+									"command": schema.StringAttribute{
+										Required: true,
+									},
+									"env": schema.MapAttribute{
+										ElementType: types.StringType,
+										Optional:    true,
+									},
+									"args": schema.ListAttribute{
+										ElementType: types.StringType,
+										Optional:    true,
+									},
+								},
+							},
+							Optional: true,
+						},
 					},
 				},
 			},
@@ -131,9 +154,6 @@ func (p *helmProvider) Schema(ctx context.Context, _ provider.SchemaRequest, res
 
 // Configure prepares a HashiCups API client for data sources and resources.
 func (p *helmProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
-	test := path.Path{
-		steps: path.PathSteps,
-	}
 
 	req.Config.Schema.GetBlocks()
 }
@@ -146,4 +166,33 @@ func (p *helmProvider) DataSources(_ context.Context) []func() datasource.DataSo
 // Resources defines the resources implemented in the provider.
 func (p *helmProvider) Resources(_ context.Context) []func() resource.Resource {
 	return nil
+}
+
+type versionValidator struct{}
+
+// Description returns a plain text description of the validator's behavior, suitable for a practitioner to understand its impact.
+func (v versionValidator) Description(ctx context.Context) string {
+	return fmt.Sprintf("validates whether api_version is alphav1")
+}
+
+// MarkdownDescription returns a markdown formatted description of the validator's behavior, suitable for a practitioner to understand its impact.
+func (v versionValidator) MarkdownDescription(ctx context.Context) string {
+	return fmt.Sprintf("validates whether api_version is alphav1")
+}
+
+// Validate runs the main validation logic of the validator, reading configuration data out of `req` and updating `resp` with diagnostics.
+func (v versionValidator) ValidateString(ctx context.Context, req validator.StringRequest, resp *validator.StringResponse) {
+	// If the value is unknown or null, there is nothing to validate.
+	if req.ConfigValue.IsUnknown() || req.ConfigValue.IsNull() {
+		return
+	}
+
+	if req.ConfigValue.ValueString() == "client.authentication.k8s.io/v1alpha1" {
+		resp.Diagnostics.AddAttributeError(
+			req.Path,
+			"v1alpha1 of the client authentication API has been removed, use v1beta1 or above",
+			"v1alpha1 of the client authentication API is removed in Kubernetes client versions 1.24 and above. You may need to update your exec plugin to use the latest version.")
+
+		return
+	}
 }
