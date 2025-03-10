e4221e22e739f345c2816aad7a7956b311d9167e
Liz Frost <lfrost@pagerduty.com>
2025-03-10 16:48:44 +0000

    Support helm's new TakeOwnership sdk value

--- helm/resource_helm_release.go
+++ helm/resource_helm_release.go
@@ -99,6 +99,7 @@ type HelmReleaseModel struct {
 	SetSensitive             types.List       `tfsdk:"set_sensitive"`
 	SkipCrds                 types.Bool       `tfsdk:"skip_crds"`
 	Status                   types.String     `tfsdk:"status"`
+	TakeOwnership            types.Bool       `tfsdk:"take_ownership"`
 	Timeout                  types.Int64      `tfsdk:"timeout"`
 	Values                   types.List       `tfsdk:"values"`
 	Verify                   types.Bool       `tfsdk:"verify"`
@@ -125,6 +126,7 @@ var defaultAttributes = map[string]interface{}{
 	"reset_values":               false,
 	"reuse_values":               false,
 	"skip_crds":                  false,
+	"take_ownership":             false,
 	"timeout":                    int64(300),
 	"verify":                     false,
 	"wait":                       true,
@@ -478,6 +480,11 @@ func (r *HelmRelease) Schema(ctx context.Context, req resource.SchemaRequest, re
 				Computed:    true,
 				Description: "Status of the release",
 			},
+			"take_ownership": schema.BoolAttribute{
+				Optional: true,
+				Default: booldefault.StaticBool(defaultAttributes["take_ownership"].(bool)),
+				Description: "Allow helm to adopt existing resources",
+			},
 			"timeout": schema.Int64Attribute{
 				Optional:    true,
 				Computed:    true,
@@ -774,6 +781,7 @@ func (r *HelmRelease) Create(ctx context.Context, req resource.CreateRequest, re
 	client.Replace = state.Replace.ValueBool()
 	client.Description = state.Description.ValueString()
 	client.CreateNamespace = state.CreateNamespace.ValueBool()
+	client.TakeOwnership = state.TakeOwnership.ValueBool()
 
 	if state.PostRender != nil {
 		binaryPath := state.PostRender.BinaryPath.ValueString()
@@ -1819,6 +1827,7 @@ func (r *HelmRelease) ModifyPlan(ctx context.Context, req resource.ModifyPlanReq
 			install.Description = plan.Description.ValueString()
 			install.CreateNamespace = plan.CreateNamespace.ValueBool()
 			install.PostRenderer = client.PostRenderer
+			install.TakeOwnership = plan.TakeOwnership.ValueBool()
 
 			values, diags := getValues(ctx, &plan)
 			resp.Diagnostics.Append(diags...)
